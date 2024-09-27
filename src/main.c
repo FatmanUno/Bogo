da4afe819c7605f2aeffc6f0a8754f334bb9dd03
FatmanUno <ariasdiego637@gmail.com>
2024-09-27 14:31:52 -0300

    El programa muesta un bogosort en pantalla, contando cuantas iteraciones lleva

--- src/main.c
+++ src/main.c
@@ -2,8 +2,11 @@
 #include "funciones.h"
 
 int main(int argc, char **argv) {
-    iniciarVentana(argc, argv);
-    glutDisplayFunc(display); 
-    glutMainLoop(); 
+    srand(time(NULL)); // Inicializa el generador de números aleatorios
+
+    iniciarVentana(argc, argv);  // Inicializa la ventana
+    glutDisplayFunc(display);    // Registra la función de renderizado
+    glutMainLoop();              // Inicia el bucle principal de GLUT
+
     return 0;
 }
